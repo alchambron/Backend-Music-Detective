e666e6432ddf02e722efb5d09b6c70153bae70c7
alchambron <alexis.chambron@gmail.com>
2023-03-07 13:52:11 +0100

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   resources :playlist_contents
   resources :playlists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'add_song', to: "playlist_contents#add_song_playlist_content"
 end
