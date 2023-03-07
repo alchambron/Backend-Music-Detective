c89c2af8c2552bdb739dc548330b4a679d526e9e
alchambron <alexis.chambron@gmail.com>
2023-03-07 13:52:02 +0100

    Adding first step of fetching and save playlist

--- app/controllers/playlist_contents_controller.rb
+++ app/controllers/playlist_contents_controller.rb
@@ -1,5 +1,29 @@
 class PlaylistContentsController < ApplicationController
-  before_action :set_playlist_content, only: %i[ show update destroy ]
+  before_action :set_playlist_content, only: %i[show update destroy]
+  require 'yt'
+
+  Yt.configuration.api_key = 'AIzaSyCFT7vH21ACuKywAblX0SzeD4uAA2594JI'
+
+  def add_song_playlist_content
+    def get_playlist_videos(playlist_url, playlist_id)
+      playlist = Yt::Playlist.new id: playlist_url
+
+      playlist.playlist_items.each do |video|
+        existing_item = PlaylistContent.find_by(youtube_title: video.title)
+
+        if existing_item
+          puts 'nope'
+        else
+          PlaylistContent.create(youtube_title: video.title, playlist_id: playlist_id)
+        end
+      end
+    end
+
+    # Parcourt toutes les playlists de manière asynchrone
+    Playlist.all.each do |content|
+      get_playlist_videos(content.playlist_url, content.id).load_async
+    end
+  end
 
   # GET /playlist_contents
   def index
@@ -39,13 +63,14 @@ class PlaylistContentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_playlist_content
-      @playlist_content = PlaylistContent.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def playlist_content_params
-      params.require(:playlist_content).permit(:youtube_title, :youtube_id, :playlist_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_playlist_content
+    @playlist_content = PlaylistContent.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def playlist_content_params
+    params.require(:playlist_content).permit(:youtube_title, :youtube_id, :playlist_id)
+  end
 end
