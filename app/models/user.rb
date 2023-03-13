4caee771216c4d157f3f223e63eada6f820eaa79
thimibka <fseetp@gmail.com>
2023-03-13 18:29:30 +0100

    create nick name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-devise :database_authenticatable, :registerable,
-	:jwt_authenticatable,
-	jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :registerable,
+    :jwt_authenticatable,
+    jwt_revocation_strategy: JwtDenylist
+
+  before_save :set_nickname
+
+  private
+
+  def set_nickname
+    self.nickname = Faker::Superhero.descriptor
+  end
 end
