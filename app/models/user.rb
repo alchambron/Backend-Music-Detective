dc3871ae411e2abd8e48d8f3d111794b6a300b29
Thomas Raducha <thomasraducha@gmail.com>
2023-03-20 10:56:53 +0100

    updated method set_nickname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
- 
 
   before_save :set_nickname
   validates :email, uniqueness: true
@@ -8,7 +7,7 @@ class User < ApplicationRecord
   private
 
   def set_nickname
-    self.nickname = Faker::Superhero.descriptor
+    self.nickname = Faker::Superhero.descriptor if nickname.nil? || nickname == Faker::Superhero.descriptor
   end
 
   after_create :welcome_send
