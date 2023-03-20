9b4c89ab5f2b09a49028edebab0017eac4913fa7
alchambron <alexis.chambron@gmail.com>
2023-03-20 17:05:37 +0100

    adding route to choice playlist

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :playlists
   get 'add_song', to: "playlist_contents#add_song_playlist_content"
   get '/member-data', to: 'members#show'
+  post '/get_playlist_data', to: 'playlist_contents#get_playlist_data'
 end
 
   
