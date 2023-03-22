97a009efec27e2f9d0e788e03db86c0a0ff98509
alchambron <alexis.chambron@gmail.com>
2023-03-22 13:04:02 +0100

    adding column to games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_150309) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_100203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_150309) do
     t.integer "score", default: 100, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user"
   end
 
   create_table "jwt_denylist", force: :cascade do |t|
